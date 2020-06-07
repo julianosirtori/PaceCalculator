fabac29245039225b6baf1e0b1ca9b9d58be6819
Juliano Sirtori <julianosirtori@gmail.com>
2020-06-07 18:44:56 -0300

    Add styled page preditor

--- src/pages/Predict/index.js
+++ src/pages/Predict/index.js
@@ -2,11 +2,42 @@ import React from 'react';
 
 import HeaderTitle from '../../components/HeaderTitle';
 import Background from '../../components/Background';
-// import { Container } from './styles';
+import PreditorImage from '../../assets/img/preditor.png';
+
+import {
+  CardContent,
+  HeaderCard,
+  Row,
+  InputStyled,
+  ButtonCalc,
+  ContentBody,
+  FirstInfo,
+  ImageCenter,
+  TextInfo,
+
+} from './styles';
 
 const Predict = () => (
   <Background>
     <HeaderTitle>Preditor de tempo</HeaderTitle>
+    <CardContent>
+      <HeaderCard>
+        <Row>
+          <InputStyled type="kmh" placeholder="Km" label="Eu corri a distancia:" />
+          <InputStyled type="time" placeholder=" hh:mm:ss" label="No tempo de:" />
+        </Row>
+        <Row>
+          <InputStyled type="kmh" placeholder="Km" label="Eu corri a distancia:" />
+          <ButtonCalc>Calcular Tempo</ButtonCalc>
+        </Row>
+      </HeaderCard>
+      <ContentBody>
+        <FirstInfo>
+          <ImageCenter source={PreditorImage} />
+          <TextInfo>Insira o Tempo e a Distancia para aparecer os ritmos de volta aqui</TextInfo>
+        </FirstInfo>
+      </ContentBody>
+    </CardContent>
   </Background>
 );
 
--- src/pages/Predict/styles.js
+++ src/pages/Predict/styles.js
@@ -1,5 +1,59 @@
 import styled from 'styled-components/native';
+import Input from '../../components/Input';
+import Card from '../../components/Card';
+import Button from '../../components/Button';
 
-export const Container = styled.SafeAreaView`
-  
+export const CardContent = styled(Card)`
+  margin-bottom: 32px;
+  padding: 16px;
+  padding-right: 0px;
+  margin-right: 16px;
+  margin-left: 16px;
+`;
+
+export const HeaderCard = styled.View`
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+export const Row = styled.View`
+  width: 100%;
+  flex-direction: row;
+  margin-bottom: 12px;
+  align-items: flex-end;
+
+`;
+
+export const InputStyled = styled(Input)`
+  margin-right: 16px;
+`;
+
+export const ButtonCalc = styled(Button)`
+  width: 120px;
+  margin-right: 16px;
+`;
+
+
+export const ContentBody = styled.View`
+  width: 100%;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FirstInfo = styled.View`
+  flex-direction: column;
+  margin-top: 32px;
+  margin-bottom: 32px;
+`;
+
+export const ImageCenter = styled.Image`
+  width: 160px;
+  height: 110px;
+  margin: auto;
+`;
+
+export const TextInfo = styled.Text`
+   width: 220px;
+   text-align: center;
 `;
